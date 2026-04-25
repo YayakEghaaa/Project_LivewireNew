f2e0374d26cb4c335e94b42dea3b04734a6d6f0c
YayakEghaaa <152347170+YayakEghaaa@users.noreply.github.com>
2026-04-25 16:27:37 +0700

    Add files via upload

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\MonitoringPanelProvider::class,
+    App\Providers\Filament\OpdPanelProvider::class,
+    App\Providers\Filament\VerifikatorPanelProvider::class,
+];
